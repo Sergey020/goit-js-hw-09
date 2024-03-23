b539e2c3c13b68a870c5f084de21e4d4d138a62c
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-03-23 15:28:36 +0200

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,36 @@
-console.log("Form");
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+const localKey = 'feedback-form-state';
+let formDataEl = {};
+
+fillFormFields();
+
+formEl.addEventListener(
+'input',
+event => {
+formDataEl[event.target.name] = event.target.value;
+localStorage.setItem(localKey, JSON.stringify(formDataEl));
+}
+);
+
+formEl.addEventListener('submit', event => {
+event.preventDefault();
+if (inputEl.value !== '' && textareaEl.value !== '') {
+console.log(formDataEl);
+localStorage.removeItem(localKey);
+event.currentTarget.reset();
+return;
+}
+alert('All fields must be filled!');
+});
+
+function fillFormFields() {
+const saveFormFields = localStorage.getItem(localKey);
+if (saveFormFields) {
+formDataEl = JSON.parse(saveFormFields);
+inputEl.value = formDataEl.email || '';
+textareaEl.value = formDataEl.message || '';
+}
+}
