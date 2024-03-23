15fead30c14d8fe1d6329f2918997e8d7c637b68
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-03-23 12:57:10 +0200

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,8 +1,1 @@
-//Описан в документации
-import SimpleLightbox от "simplelightbox";
-// Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-console.log(simpleLightbox);
